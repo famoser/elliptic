b2431f984c08ddfd6f954ee54c69c5af6dfe0b44
Florian Moser <git@famoser.ch>
2025-03-10 22:12:04 +0100

    ref: Split test into multiple files, cleanup and simplify

---
+++ tests/integration/Spec/EcdsaSignTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Mdanter\Ecc\Integration\Spec;
+
+use Mdanter\Ecc\Crypto\Signature\Signer;
+use Mdanter\Ecc\Crypto\Signature\SignHasher;
+use Mdanter\Ecc\Curves\CurveFactory;
+use Mdanter\Ecc\Primitives\GeneratorPoint;
+use PHPUnit\Framework\TestCase;
+
+class EcdsaSignTest extends TestCase
+{
+    public function getEcdsaSignFixtures(): array
+    {
+        $files = FixturesRepository::read('ecdsa');
+        $datasets = [];
+
+        foreach ($files as $file) {
+            $generator = CurveFactory::getGeneratorByName($file['curve']);
+            foreach ($file['fixtures'] as $i => $fixture) {
+                $datasetIdentifier = $file['file'] . "." . $i;
+
+                $plaintextSet = array_key_exists('msg_full', $fixture) && array_key_exists('algo', $fixture);
+                $hashSet = array_key_exists('msg', $fixture);
+                if ((!$plaintextSet && !$hashSet) || ($hashSet && $plaintextSet)) {
+                    throw new \RuntimeException("Defined EITHER the raw hash value (msg), or the plain and hash algorithm (msg_full and algo). dataset: ". $datasetIdentifier);
+                }
+
+                $message = $hashSet ? gmp_init($fixture['msg'], 16) : null;
+                if ($plaintextSet) {
+                    $msg = hex2bin($fixture['msg_full']);
+                    $hasher = new SignHasher($fixture['algo']);
+                    $message = $hasher->makeHash($msg, $generator);
+                }
+
+
+                $datasets[$datasetIdentifier] = [
+                    $generator,
+                    gmp_init($fixture['private'], 10),
+                    gmp_init($fixture['k'], 16),
+                    gmp_init($fixture['r'], 16),
+                    gmp_init($fixture['s'], 16),
+                    $message,
+                ];
+            }
+        }
+
+        return $datasets;
+    }
+
+
+    /**
+     * @dataProvider getEcdsaSignFixtures
+     */
+    public function testEcdsaSign(GeneratorPoint $G, \GMP $privateKeyMultiplier, \GMP $k, \GMP $eR, \GMP $eS, \GMP $message)
+    {
+        $math = $G->getAdapter();
+        $signer = new Signer($math);
+
+        $privateKey = $G->getPrivateKeyFrom($privateKeyMultiplier);
+        $sig = $signer->sign($privateKey, $message, $k);
+
+        $this->assertEquals($eR, $sig->getR());
+        $this->assertEquals($eS, $sig->getS());
+
+        $this->assertTrue($signer->verify($privateKey->getPublicKey(), $sig, $message));
+    }
+}
---
+++ tests/integration/Spec/KeypairTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Mdanter\Ecc\Integration\Spec;
+
+use Mdanter\Ecc\Curves\CurveFactory;
+use Mdanter\Ecc\Primitives\GeneratorPoint;
+use Mdanter\Ecc\Serializer\Point\Format\CompressedPointSerializer;
+use Mdanter\Ecc\Serializer\Point\Format\UncompressedPointSerializer;
+use PHPUnit\Framework\TestCase;
+
+class KeypairTest extends TestCase
+{
+    /**
+     * @return array
+     */
+    public function getKeypairFixtures(): array
+    {
+        $files = FixturesRepository::read('keypairs');
+        $datasets = [];
+
+        foreach ($files as $file) {
+            $generator = CurveFactory::getGeneratorByName($file['curve']);
+            foreach ($file['fixtures'] as $i => $fixture) {
+                $datasetIdentifier = $file['file'] . "." . $i;
+
+                $datasets[$datasetIdentifier] = [
+                    $generator,
+                    gmp_init((string)$fixture['k'], 10),
+                    gmp_init($fixture['x'], 16),
+                    gmp_init($fixture['y'], 16)
+                ];
+            }
+        }
+
+        return $datasets;
+    }
+
+    /**
+     * @dataProvider getKeypairFixtures()
+     */
+    public function testGetPublicKey(GeneratorPoint $generator, \GMP $k, \GMP $expectedX, \GMP $expectedY)
+    {
+        $privateKey = $generator->getPrivateKeyFrom($k);
+        $publicKey = $privateKey->getPublicKey();
+
+        $this->assertEquals($expectedX, $publicKey->getPoint()->getX());
+        $this->assertEquals($expectedY, $publicKey->getPoint()->getY());
+    }
+
+    /**
+     * @dataProvider getKeypairFixtures()
+     */
+    public function testPointSerializers(GeneratorPoint $generator, \GMP $k, \GMP $expectedX, \GMP $expectedY)
+    {
+        $adapter = $generator->getAdapter();
+
+        $publicKey = $generator->getPublicKeyFrom($expectedX, $expectedY);
+
+        $serializer = new UncompressedPointSerializer();
+        $serialized = $serializer->serialize($publicKey->getPoint());
+        $parsed = $serializer->deserialize($generator->getCurve(), $serialized);
+        $this->assertTrue($parsed->equals($publicKey->getPoint()));
+
+        $compressingSerializer = new CompressedPointSerializer($adapter);
+        $serialized = $compressingSerializer->serialize($publicKey->getPoint());
+        $parsed = $compressingSerializer->deserialize($generator->getCurve(), $serialized);
+        $this->assertTrue($parsed->equals($publicKey->getPoint()));
+    }
+}
