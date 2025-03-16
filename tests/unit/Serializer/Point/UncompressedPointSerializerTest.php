8341907ada82bd1293440135aad620c0e23e9bcb
Florian Moser <git@famoser.ch>
2025-03-16 18:37:10 +0100

    fix: Fix regression in unit test

--- tests/unit/Serializer/Point/UncompressedPointSerializerTest.php
+++ tests/unit/Serializer/Point/UncompressedPointSerializerTest.php
@@ -5,13 +5,14 @@ namespace Mdanter\Ecc\Tests\Serializer\Point;
 
 use Mdanter\Ecc\EccFactory;
 use Mdanter\Ecc\Serializer\Point\Format\UncompressedPointSerializer;
+use Mdanter\Ecc\Serializer\Point\PointDecodingException;
 use Mdanter\Ecc\Tests\AbstractTestCase;
 
 class UncompressedPointSerializerTest extends AbstractTestCase
 {
     public function testChecksPrefix()
     {
-        $this->expectException(\InvalidArgumentException::class);
+        $this->expectException(PointDecodingException::class);
         $this->expectExceptionMessage('Invalid data: only uncompressed keys are supported.');
         $data = '01aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
         $serializer = new UncompressedPointSerializer();
