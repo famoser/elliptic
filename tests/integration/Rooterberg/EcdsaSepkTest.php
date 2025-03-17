6797ecc1d38a2f02fb3d4fb58f06e9a1be93f4af
Florian Moser <git@famoser.ch>
2025-03-17 10:59:18 +0100

    test: Add rooterberg testsuite

---
+++ tests/integration/Rooterberg/EcdsaSepkTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mdanter\Ecc\Integration\Rooterberg;
+
+use Mdanter\Ecc\Crypto\Key\PublicKeyInterface;
+use Mdanter\Ecc\Crypto\Signature\Signature;
+use Mdanter\Ecc\Crypto\Signature\Signer;
+use Mdanter\Ecc\Curves\SecpCurves;
+use Mdanter\Ecc\Math\GmpMath;
+use Mdanter\Ecc\Primitives\GeneratorPoint;
+use Mdanter\Ecc\Serializer\Point\PointSize;
+use PHPUnit\Framework\TestCase;
+
+class EcdsaSepkTest extends TestCase
+{
+    public static function provideSecp224k1(): array
+    {
+        return FixturesRepository::createEcdsaSha224Fixtures('secp224k1');
+    }
+
+    /**
+     * @dataProvider provideSecp224k1
+     */
+    public function testSecp224k1(GeneratorPoint $generator, PublicKeyInterface $publicKey, \GMP $hash, string $signature, string $comment, bool $valid, array $flags): void
+    {
+        $generator = SecpCurves::create()->generator224k1();
+
+        $this->testCurve($generator, $publicKey, $hash, $signature, $comment, $valid, $flags);
+    }
+
+    protected function testCurve(GeneratorPoint $generator, PublicKeyInterface $publicKey, \GMP $hash, string $signature, string $comment, bool $valid, array $flags): void
+    {
+        // crude signature validity check, as this is not our prime concern here
+        $integerHexLength = PointSize::getByteSize($generator->getCurve()) * 2;
+        if (strlen($signature) !== $integerHexLength*2) {
+            $this->assertFalse($valid);
+            return;
+        }
+
+        // unserialize signature
+        $r = gmp_init(substr($signature, 0, $integerHexLength), 16);
+        $s = gmp_init(substr($signature, $integerHexLength), 16);
+        $signature = new Signature($r, $s);
+
+        // verify signature
+        $signer = new Signer(new GmpMath());
+        $verified = $signer->verify($publicKey, $signature, $hash);
+
+        // check congruent with Wyche proof expectation
+        if ($verified) {
+            $this->assertTrue($valid);
+        } else {
+            $this->assertFalse($valid);
+        }
+    }
+}
