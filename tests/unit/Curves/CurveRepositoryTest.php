fe4702d692381292d628d29779cb388f45eba376
Florian Moser <git@famoser.ch>
2025-03-19 21:22:09 +0100

    test: Add test for curve repository

---
+++ tests/unit/Curves/CurveRepositoryTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Mdanter\Ecc\Tests\Curves;
+
+use Mdanter\Ecc\Curves\CurveRepository;
+use PHPUnit\Framework\TestCase;
+
+class CurveRepositoryTest extends TestCase
+{
+    public function testAllKnownNamesReturnCurves()
+    {
+        $curveRepository = new CurveRepository();
+
+        foreach ($curveRepository->getKnownCurveOIDs() as $knownCurveOID) {
+            $this->assertNotNull($curveRepository->findByOID($knownCurveOID));
+        }
+    }
+
+    public function testAllKnownOIDsReturnCurves()
+    {
+        $curveRepository = new CurveRepository();
+
+        foreach ($curveRepository->getKnownCurveOIDs() as $knownCurveOID) {
+            $this->assertNotNull($curveRepository->findByOID($knownCurveOID));
+        }
+    }
+
+    public function testSingleInstanceOfCurves()
+    {
+        $curveRepository = new CurveRepository();
+
+        $someCurveOID = $curveRepository->getKnownCurveOIDs()[0];
+        $curve1 = $curveRepository->findByOID($someCurveOID);
+        $curve2 = $curveRepository->findByOID($someCurveOID);
+
+        $this->assertEquals($curve1, $curve2);
+    }
+}
