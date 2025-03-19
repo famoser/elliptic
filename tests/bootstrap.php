fd173db0cad1262fafb57b96aa2599d420a24ecc
Florian Moser <git@famoser.ch>
2025-03-19 21:24:40 +0100

    test: Cleanup obsolete data and config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,38 +1,4 @@
 <?php
 
-function buildPath()
-{
-    return implode(DIRECTORY_SEPARATOR, func_get_args());
-}
+include implode(DIRECTORY_SEPARATOR, [__DIR__, '..', 'vendor', 'autoload.php']);
 
-gc_disable();
-
-define('TEST_DATA_DIR', buildPath(__DIR__, 'data'));
-
-include buildPath(__DIR__, '..', 'vendor', 'autoload.php');
-
-$requiredNestingLevel = 150;
-
-if (extension_loaded('xdebug')) {
-    $currentNestingLevel = intval(ini_get('xdebug.max_nesting_level'));
-
-    if ($currentNestingLevel < $requiredNestingLevel) {
-        fwrite(STDERR, <<<TEXT
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-IMPORTANT NOTICE:
-It seems like you have the xdebug extension loaded.
-To use phpecc you have to increase the maximum nesting level from $currentNestingLevel to at least $requiredNestingLevel
-You can do this by adding the following line to your php.ini:
-
-xdebug.max_nesting_level = $requiredNestingLevel
-
-The tests will now run with this setting.
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-TEXT
-        );
-        ini_set('xdebug.max_nesting_level', $requiredNestingLevel);
-    }
-}
