6a0b9413141de9d2b96dd9e3d9a85c864e7a32fd
Florian Moser <git@famoser.ch>
2025-04-27 22:10:25 +0200

    test: Sanity-check affine add functiom

--- src/Math/Primitives/JacobiPoint.php
+++ src/Math/Primitives/JacobiPoint.php
@@ -17,4 +17,9 @@ class JacobiPoint
     {
         return gmp_cmp($this->Z, 0) === 0;
     }
+
+    public function equals(JacobiPoint $other): bool
+    {
+        return gmp_cmp($this->X, $other->X) === 0 && gmp_cmp($this->Y, $other->Y) === 0 && gmp_cmp($this->Z, $other->Z) === 0;
+    }
 }
---
+++ tests/unit/Math/Calculator/SW_ANeg3_CalculatorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Famoser\Elliptic\Tests\Math\Calculator;
+
+use Famoser\Elliptic\Curves\SEC2CurveFactory;
+use Famoser\Elliptic\Math\Calculator\SW_ANeg3_Calculator;
+use PHPUnit\Framework\TestCase;
+
+class SW_ANeg3_CalculatorTest extends TestCase
+{
+    public function testAffineAddSanity(): void
+    {
+        $curve = SEC2CurveFactory::secp192r1();
+        $calculator = new SW_ANeg3_Calculator($curve);
+
+        $GNative = $calculator->affineToNative($curve->getG());
+        $expectedResult = $calculator->add($GNative, $GNative);
+        $actualResult = $calculator->addAffine($GNative, $curve->getG());
+
+        $this->assertTrue($expectedResult->equals($actualResult));
+    }
+}
