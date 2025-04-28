2636db939d1752773024036c24cfc1c1c084b489
Florian Moser <git@famoser.ch>
2025-04-28 08:45:56 +0200

    fix: Handle failed inversion

--- src/Math/Calculator/Adder/SWUnsafeAdder.php
+++ src/Math/Calculator/Adder/SWUnsafeAdder.php
@@ -34,7 +34,6 @@ trait SWUnsafeAdder
         // rule 4 (note that a / b = a * b^-1)
         $lambda = $this->field->mul(
             gmp_sub($b->y, $a->y),
-            /** @phpstan-ignore-next-line invert may return false; then this will crash (which is OK, because cannot recover anyway) */
             $this->field->invert(gmp_sub($b->x, $a->x))
         );
 
@@ -72,7 +71,6 @@ trait SWUnsafeAdder
                 ),
                 $this->curve->getA()
             ),
-            /** @phpstan-ignore-next-line invert may return false; then this will crash (which is OK, because cannot recover anyway) */
             $this->field->invert(
                 gmp_mul(2, $a->y)
             )
