078371ec73d3d9acfabe024afe66f9fd348181a7
Florian Moser <git@famoser.ch>
2025-07-03 07:45:49 +0200

    feat: Add XPoint structure

---
+++ src/Math/Calculator/MGCalculator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Famoser\Elliptic\Math\Calculator;
+
+use Famoser\Elliptic\Math\Calculator\Adder\MGUnsafeAdder;
+use Famoser\Elliptic\Math\Calculator\Coordinator\PointCoordinator;
+use Famoser\Elliptic\Math\Calculator\Coordinator\XZCoordinator;
+use Famoser\Elliptic\Math\Calculator\Multiplicator\DoubleAndAddAlwaysMultiplicator;
+use Famoser\Elliptic\Math\Calculator\Swapper\PointSwapper;
+use Famoser\Elliptic\Primitives\Curve;
+use Famoser\Elliptic\Primitives\CurveType;
+use Famoser\Elliptic\Primitives\Point;
+
+/**
+ * Calculator for montgomery curves (adder not implemented yet)
+ */
+abstract class MGCalculator extends AbstractCalculator
+{
+    use XZCoordinator;
+    use PointSwapper;
+    /** @use DoubleAndAddAlwaysMultiplicator<Point> */
+    use DoubleAndAddAlwaysMultiplicator;
+
+    public function __construct(Curve $curve)
+    {
+        parent::__construct($curve);
+
+        // check allowed to use this calculator
+        $check = $curve->getType() === CurveType::Montgomery;
+        if (!$check) {
+            throw new \AssertionError('Cannot use this calculator with the chosen curve.');
+        }
+    }
+}
