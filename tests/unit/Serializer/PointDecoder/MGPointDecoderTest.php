f8383a08a534fd2e7170e9454a1fd155b7d7dea2
Florian Moser <git@famoser.ch>
2025-07-17 14:47:52 +0200

    test: Add more tests

---
+++ tests/unit/Serializer/PointDecoder/MGPointDecoderTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Famoser\Elliptic\Tests\Serializer\PointDecoder;
+
+use Famoser\Elliptic\Curves\BernsteinCurveFactory;
+use Famoser\Elliptic\Primitives\Curve;
+use Famoser\Elliptic\Primitives\CurveType;
+use Famoser\Elliptic\Serializer\PointDecoder\MGPointDecoder;
+use Famoser\Elliptic\Serializer\PointDecoder\PointDecoderException;
+use Famoser\Elliptic\Tests\TestUtils\CurveBuilder;
+use PHPUnit\Framework\TestCase;
+
+class MGPointDecoderTest extends TestCase
+{
+    /**
+     * @throws PointDecoderException
+     */
+    public function testFromCoordinatesCreatesPoint(): void
+    {
+        $curve = BernsteinCurveFactory::curve25519();
+        $decoder = new MGPointDecoder($curve);
+
+        $expectedPoint = $curve->getG();
+        $actualPoint = $decoder->fromCoordinates($expectedPoint->x, $expectedPoint->y);
+
+        $this->assertTrue($expectedPoint->equals($actualPoint));
+    }
+
+    public static function invalidXYPoints(): array
+    {
+        $curve = BernsteinCurveFactory::curve25519();
+        $expectedPoint = $curve->getG();
+
+        $one = gmp_init(1);
+        return [
+            [$curve, $one, $expectedPoint->y],
+            [$curve, $expectedPoint->x, $one],
+            [$curve, $one, $one],
+        ];
+    }
+
+    /**
+     * @dataProvider invalidXYPoints
+     */
+    public function testFromCoordinatesChecksCurveEquation(Curve $curve, \GMP $x, \GMP $y): void
+    {
+        $this->expectException(PointDecoderException::class);
+
+        $decoder = new MGPointDecoder($curve);
+
+        $decoder->fromCoordinates($x, $y);
+    }
+
+    public function testFromCoordinatesChecksCurveType(): void
+    {
+        $curve = BernsteinCurveFactory::curve25519();
+        $unsupportedCurve = (new CurveBuilder($curve))->withType(CurveType::ShortWeierstrass)->build();
+
+        $this->expectException(\AssertionError::class);
+
+        new MGPointDecoder($unsupportedCurve);
+    }
+
+    /**
+     * @throws PointDecoderException
+     */
+    public function testFromXCoordinatesCreatesPoint(): void
+    {
+        $curve = BernsteinCurveFactory::curve25519();
+        $decoder = new MGPointDecoder($curve);
+
+        $actualPoint = $decoder->fromXCoordinate($curve->getG()->x);
+
+        $this->assertNotNull($actualPoint);
+    }
+
+    /**
+     * @throws PointDecoderException
+     */
+    public function testFromXCoordinateRejectsInvalidXCoordinates(): void
+    {
+        $this->expectException(PointDecoderException::class);
+
+        $curve = BernsteinCurveFactory::curve25519();
+        $decoder = new MGPointDecoder($curve);
+
+        $decoder->fromXCoordinate(gmp_init(2));
+    }
+}
---
+++ tests/unit/Serializer/SECSerializerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Famoser\Elliptic\Tests\Serializer;
+
+use Famoser\Elliptic\Curves\SEC2CurveFactory;
+use Famoser\Elliptic\Primitives\Point;
+use Famoser\Elliptic\Serializer\PointDecoder\SWPointDecoder;
+use Famoser\Elliptic\Serializer\SEC\SECEncoding;
+use Famoser\Elliptic\Serializer\SECSerializer;
+use PHPUnit\Framework\TestCase;
+
+class SECSerializerTest extends TestCase
+{
+    public function testCompressedPoint(): void
+    {
+        $curve = SEC2CurveFactory::secp192r1();
+        $decoder = new SWPointDecoder($curve);
+        $serializer = new SECSerializer($curve, $decoder, SECEncoding::COMPRESSED);
+
+        $expectedPoint = $curve->getG();
+        $serializedPoint = $serializer->serialize($expectedPoint);
+        $actualPoint = $serializer->deserialize($serializedPoint);
+
+        $this->assertEquals($expectedPoint, $actualPoint);
+    }
+
+    public function testUncompressedPoint(): void
+    {
+        $curve = SEC2CurveFactory::secp192r1();
+        $decoder = new SWPointDecoder($curve);
+        $serializer = new SECSerializer($curve, $decoder, SECEncoding::UNCOMPRESSED);
+
+        $expectedPoint = $curve->getG();
+        $serializedPoint = $serializer->serialize($expectedPoint);
+        $actualPoint = $serializer->deserialize($serializedPoint);
+
+        $this->assertEquals($expectedPoint, $actualPoint);
+    }
+
+    public function testInfinityPoint(): void
+    {
+        $curve = SEC2CurveFactory::secp192r1();
+        $decoder = new SWPointDecoder($curve);
+        $serializer = new SECSerializer($curve, $decoder);
+
+        $infinity = Point::createInfinity();
+        $serializedPoint = $serializer->serialize($infinity);
+        $actualPoint = $serializer->deserialize($serializedPoint);
+
+        $this->assertEquals($infinity, $actualPoint);
+    }
+}
