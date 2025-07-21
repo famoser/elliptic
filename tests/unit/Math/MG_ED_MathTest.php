558d241e760fdb7bcaeb17470f91077319a25ddb
Florian Moser <git@famoser.ch>
2025-07-21 15:28:52 +0200

    test: Add dedicated unit tests for TwED and ED math

---
+++ tests/unit/Math/MG_ED_MathTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Famoser\Elliptic\Tests\Math;
+
+use Famoser\Elliptic\Curves\BernsteinCurveFactory;
+use Famoser\Elliptic\Math\Calculator\MGXCalculator;
+use Famoser\Elliptic\Math\MG_ED_Math;
+use Famoser\Elliptic\Math\MG_TwED_Math;
+use Famoser\Elliptic\Serializer\Decoder\RFC7784Decoder;
+use Famoser\Elliptic\Serializer\PointDecoder\MGPointDecoder;
+use Famoser\Elliptic\Serializer\PointDecoder\PointDecoderException;
+use Famoser\Elliptic\Tests\Math\Calculator\RFC7784TestVectorsTrait;
+use PHPUnit\Framework\TestCase;
+
+class MG_ED_MathTest extends TestCase
+{
+    use RFC7784TestVectorsTrait;
+
+    /**
+     * @dataProvider vectors448
+     */
+    public function testTestVectors448OnEd(string $scalar, string $u, string $expectedResult): void
+    {
+        $curve = BernsteinCurveFactory::curve448();
+        $birationalMapping = BernsteinCurveFactory::curve448ToEdwards();
+        $edwardsCurve = BernsteinCurveFactory::curve448Edwards();
+        $calculator = new MG_ED_Math($curve, $birationalMapping, $edwardsCurve);
+
+        $decoder = new RFC7784Decoder();
+        $scalar = $decoder->decodeScalar448($scalar);
+        $u = $decoder->decodeUCoordinate($u, 448);
+
+        try {
+            $pointDecoder = new MGPointDecoder($curve);
+            $uPoint = $pointDecoder->fromXCoordinate($u, true);
+        } catch (PointDecoderException) {
+            $this->markTestSkipped();
+        }
+
+        $result = $calculator->mul($uPoint, $scalar);
+        $encodedResult = $decoder->encodeUCoordinate($result->x, 448);
+        $this->assertEquals($expectedResult, $encodedResult);
+    }
+}
---
+++ tests/unit/Math/MG_TwED_MathTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Famoser\Elliptic\Tests\Math;
+
+use Famoser\Elliptic\Curves\BernsteinCurveFactory;
+use Famoser\Elliptic\Math\Calculator\MGXCalculator;
+use Famoser\Elliptic\Math\MG_ED_Math;
+use Famoser\Elliptic\Math\MG_TwED_Math;
+use Famoser\Elliptic\Serializer\Decoder\RFC7784Decoder;
+use Famoser\Elliptic\Serializer\PointDecoder\MGPointDecoder;
+use Famoser\Elliptic\Serializer\PointDecoder\PointDecoderException;
+use Famoser\Elliptic\Tests\Math\Calculator\RFC7784TestVectorsTrait;
+use PHPUnit\Framework\TestCase;
+
+class MG_TwED_MathTest extends TestCase
+{
+    use RFC7784TestVectorsTrait;
+
+    /**
+     * @dataProvider vectors25519
+     */
+    public function testTestVectors25519OnTwED(string $scalar, string $u, string $expectedResult): void
+    {
+        $curve = BernsteinCurveFactory::curve25519();
+        $birationalMapping = BernsteinCurveFactory::curve25519ToEdwards25519();
+        $twistedEdwardsCurve = BernsteinCurveFactory::edwards25519();
+        $calculator = new MG_TwED_Math($curve, $birationalMapping, $twistedEdwardsCurve);
+
+        $decoder = new RFC7784Decoder();
+        $scalar = $decoder->decodeScalar25519($scalar);
+        $u = $decoder->decodeUCoordinate($u, 255);
+
+        try {
+            $mgDecoder = new MGPointDecoder($curve);
+            $uPoint = $mgDecoder->fromXCoordinate($u, true);
+        } catch (PointDecoderException) {
+            $this->markTestSkipped();
+        }
+
+        $result = $calculator->mul($uPoint, $scalar);
+        $encodedResult = $decoder->encodeUCoordinate($result->x, 255);
+        $this->assertEquals($expectedResult, $encodedResult);
+    }
+}
