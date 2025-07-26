18644cf325c5bac3a0c930bfe91b7aaffef83ded
Florian Moser <git@famoser.ch>
2025-07-26 12:32:48 +0200

    test: Test infinity behavior of primitives

--- tests/unit/Math/Primitives/InfinityTest.php
+++ tests/unit/Math/Primitives/InfinityTest.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace Famoser\Elliptic\Tests\Math;
+namespace Famoser\Elliptic\Tests\Math\Primitives;
 
 use Famoser\Elliptic\Math\Primitives\JacobiPoint;
+use Famoser\Elliptic\Math\Primitives\ProjectiveCoordinates;
 use Famoser\Elliptic\Primitives\Point;
 use PHPUnit\Framework\TestCase;
 
-class PrimitivesConsistencyCheck extends TestCase
+class InfinityTest extends TestCase
 {
     public function testPointInfinity(): void
     {
@@ -17,6 +18,12 @@ class PrimitivesConsistencyCheck extends TestCase
     public function testJacobiPointInfinity(): void
     {
         $point = JacobiPoint::createInfinity();
+        $this->assertTrue($point->isInfinity());
+    }
+
+    public function testProjectiveCoordinatesPointInfinity(): void
+    {
+        $point = ProjectiveCoordinates::createInfinity();
         $this->assertTrue($point->isInfinity());
     }
 }
