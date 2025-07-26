fbfa73e4100d424461937b331e868c248bc20d43
Florian Moser <git@famoser.ch>
2025-07-26 12:33:07 +0200

    test: Remove unused tests

--- tests/unit/Math/MG_ED_MathTest.php
+++ tests/unit/Math/MG_ED_MathTest.php
@@ -41,24 +41,4 @@ class MG_ED_MathTest extends TestCase
         $encodedResult = $decoder->encodeUCoordinate($result->x, 448);
         $this->assertEquals($expectedResult, $encodedResult);
     }
-
-    public function testCurve448(): void
-    {
-        $curve = BernsteinCurveFactory::curve448();
-        $birationalMapping = BernsteinCurveFactory::curve448ToEdwards();
-        $edwardsCurve = BernsteinCurveFactory::curve448Edwards();
-        $math = new MG_ED_Math($curve, $birationalMapping, $edwardsCurve);
-        $baselineMath = new MGUnsafeMath($curve);
-
-        $factor = gmp_init('599189175373896402783756016145213256157230856085026129926891459468622403380588640249457727683869421921443004045221642549886377526240828', 10);
-        $actual = $math->mul($curve->getG(), $factor);
-        $actualBaseline = $baselineMath->mul($curve->getG(), $factor);
-        $expected = new Point(
-            gmp_init('356464349418515164077825580275797994008614129675071795735859787967236123872832230826634350987952927395289262393527422212823415563783431', 10),
-            gmp_init('130052194822882327916107803584377361768737051523812872835339292941462025016445220505950437485736657271324323672091546511573904787985802', 10)
-        );
-
-        $this->assertObjectEquals($expected, $actualBaseline);
-        $this->assertObjectEquals($expected, $actual);
-    }
 }
