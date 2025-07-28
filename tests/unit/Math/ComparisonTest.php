ffd66659b7bc177169ceec7d1b73304b417028d6
Florian Moser <git@famoser.ch>
2025-07-28 20:14:53 +0200

    test: Speed up unit tests

--- tests/unit/Math/ComparisonTest.php
+++ tests/unit/Math/ComparisonTest.php
@@ -158,32 +158,4 @@ class ComparisonTest extends TestCase
 
         $this->assertObjectEquals($expected, $actual);
     }
-
-    /**
-     * @dataProvider mathWithBaseline
-     */
-    public function testMulSameResult(string $curveName, MathInterface $math, MathInterface $baseline): void
-    {
-        $this->skipUnresolvedError(__CLASS__, __FUNCTION__, $math::class, $curveName);
-
-        $curve = $math->getCurve();
-
-        $factors = array_map(static fn($number) => gmp_mul($number, $curve->getH()), [0, 1, 2, 3]);
-        $order = gmp_mul($curve->getN(), $curve->getH());
-        $factors[4] = $order;
-        $factors[5] = gmp_sub($order, $curve->getH());
-        $factors[6] = gmp_add($order, $curve->getH());
-        $factors[7] = gmp_sub($order, gmp_mul(2312312, $curve->getH())); // random number close to group order
-
-        foreach ($factors as $i => $factor) {
-            $expected = $baseline->mul($curve->getG(), $factor);
-            $actual = $math->mul($curve->getG(), $factor);
-            if (gmp_cmp($factor, 0) === 0 || gmp_cmp($factor, $order) === 0) {
-                $this->assertTrue($baseline->isInfinity($expected));
-                $this->assertTrue($math->isInfinity($actual));
-            } else {
-                $this->assertObjectEquals($expected, $actual, 'equals', "Failed for factor " . $i . " (" . gmp_strval($factor, 16) . ")");
-            }
-        }
-    }
 }
