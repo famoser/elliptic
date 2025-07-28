f47e05bfa709c95d9becfb476cdfdc6c60aa7ea8
Florian Moser <git@famoser.ch>
2025-07-28 22:09:57 +0200

    test: Test invalid input

--- tests/unit/Serializer/SECSerializerTest.php
+++ tests/unit/Serializer/SECSerializerTest.php
@@ -4,19 +4,26 @@ namespace Famoser\Elliptic\Tests\Serializer;
 
 use Famoser\Elliptic\Curves\SEC2CurveFactory;
 use Famoser\Elliptic\Math\SWUnsafeMath;
+use Famoser\Elliptic\Primitives\Curve;
 use Famoser\Elliptic\Serializer\PointDecoder\SWPointDecoder;
 use Famoser\Elliptic\Serializer\SEC\SECEncoding;
 use Famoser\Elliptic\Serializer\SECSerializer;
+use Famoser\Elliptic\Serializer\SerializerException;
 use PHPUnit\Framework\TestCase;
 
 class SECSerializerTest extends TestCase
 {
+    private static function createSerializer(Curve $curve, SECEncoding $preferredEncoding): SECSerializer
+    {
+        $math = new SWUnsafeMath($curve);
+        $decoder = new SWPointDecoder($curve);
+        return new SECSerializer($math, $decoder, $preferredEncoding);
+    }
+
     public function testCompressedPoint(): void
     {
         $curve = SEC2CurveFactory::secp192r1();
-        $math = new SWUnsafeMath($curve);
-        $decoder = new SWPointDecoder($curve);
-        $serializer = new SECSerializer($math, $decoder, SECEncoding::COMPRESSED);
+        $serializer = self::createSerializer($curve, SECEncoding::COMPRESSED);
 
         $expectedPoint = $curve->getG();
         $serializedPoint = $serializer->serialize($expectedPoint);
@@ -28,9 +35,7 @@ class SECSerializerTest extends TestCase
     public function testUncompressedPoint(): void
     {
         $curve = SEC2CurveFactory::secp192r1();
-        $math = new SWUnsafeMath($curve);
-        $decoder = new SWPointDecoder($curve);
-        $serializer = new SECSerializer($math, $decoder, SECEncoding::UNCOMPRESSED);
+        $serializer = self::createSerializer($curve, SECEncoding::UNCOMPRESSED);
 
         $expectedPoint = $curve->getG();
         $serializedPoint = $serializer->serialize($expectedPoint);
@@ -51,5 +56,14 @@ class SECSerializerTest extends TestCase
         $actualPoint = $serializer->deserialize($serializedPoint);
 
         $this->assertTrue($actualPoint->equals($infinity));
+    }
+    
+    public function testInvalidInput(): void
+    {
+        $curve = SEC2CurveFactory::secp192r1();
+        $serializer = self::createSerializer($curve, SECEncoding::UNCOMPRESSED);
+
+        $this->expectException(SerializerException::class);;
+        $serializer->deserialize('0900023');
     }
 }
