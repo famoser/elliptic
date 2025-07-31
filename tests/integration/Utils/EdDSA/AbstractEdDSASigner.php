d68aa9d6ceee3dd840920cd3505fb9fa8a27c524
Florian Moser <git@famoser.ch>
2025-07-31 15:11:00 +0200

    feat: Add Ed448 sign verifier

--- tests/integration/Utils/EdDSA/AbstractEdDSASigner.php
+++ tests/integration/Utils/EdDSA/AbstractEdDSASigner.php
@@ -2,22 +2,23 @@
 
 declare(strict_types=1);
 
-namespace Famoser\Elliptic\Integration\Utils;
+namespace Famoser\Elliptic\Integration\Utils\EdDSA;
 
 use Famoser\Elliptic\Math\MathInterface;
 use Famoser\Elliptic\Primitives\Point;
 use Famoser\Elliptic\Serializer\Decoder\BinaryDecoder;
 use Famoser\Elliptic\Serializer\Decoder\RFC7784Decoder;
+use Famoser\Elliptic\Serializer\PointDecoder\PointDecoderInterface;
 use Famoser\Elliptic\Serializer\PointDecoder\TwEDPointDecoder;
 
 /**
- * implements Ed25519 according to RFC8032
+ * implements EdDSA according to RFC8032
  *
  * notably follows closely https://datatracker.ietf.org/doc/html/rfc8032#section-6
  */
-class EDDSASigner
+abstract class AbstractEdDSASigner
 {
-    public function __construct(private readonly MathInterface $math)
+    public function __construct(private readonly MathInterface $math, private readonly PointDecoderInterface $decoder, private readonly int $byteSize)
     {
     }
 
@@ -32,18 +33,18 @@ class EDDSASigner
 
     private function verifyInternal(string $publicKeyHex, string $signatureHex, string $messageHex): bool
     {
-        if (strlen($publicKeyHex) !== 64) {
+        if (strlen($publicKeyHex) !== $this->byteSize * 2) {
             throw new \RuntimeException("Bad public key length");
         }
-        if (strlen($signatureHex) !== 128) {
+        if (strlen($signatureHex) !== $this->byteSize * 4) {
             throw new \RuntimeException("Bad signature length");
         }
 
         $A = $this->decompressPoint($publicKeyHex);
-        $Rs = substr($signatureHex, 0, 64);
+        $Rs = substr($signatureHex, 0, $this->byteSize * 2);
         $R = $this->decompressPoint($Rs);
 
-        $ss = substr($signatureHex, 64);
+        $ss = substr($signatureHex, $this->byteSize * 2);
         $s = $this->decodeScalar($ss);
 
         $h = $this->hashToScalar($Rs . $publicKeyHex . $messageHex);
@@ -57,20 +58,19 @@ class EDDSASigner
     private function decompressPoint(string $pointHex): Point
     {
         $decoder = new RFC7784Decoder();
-        $y = $decoder->decodeUCoordinate($pointHex, 255);
+        $y = $decoder->decodeUCoordinate($pointHex, $this->byteSize * 8 - 1);
 
         $signByte = substr($pointHex, strlen($pointHex) - 2);
         $isEven = !((hexdec($signByte) >> 7 & 1));
 
-        $decoder = new TwEDPointDecoder($this->math->getCurve());
-        return $decoder->fromYCoordinate($y, $isEven);
+        return $this->decoder->fromYCoordinate($y, $isEven);
     }
 
     private function decodeScalar(string $ss): \GMP
     {
         $decoder = new BinaryDecoder();
         $b = $decoder->decodeHexToByteArray($ss);
-        $s = $decoder->decodeLittleEndian($b, 256);
+        $s = $decoder->decodeLittleEndian($b, $this->byteSize * 8);
 
         if ($s >= $this->math->getCurve()->getN()) {
             throw new \RuntimeException("s > N; abort");
@@ -81,11 +81,12 @@ class EDDSASigner
 
     private function hashToScalar(string $hex): \GMP
     {
-        $content = hex2bin($hex);
-        $digestHex = hash('sha512', $content);
+        $digestHex = $this->hash($hex);
 
         $decoder = new BinaryDecoder();
         $b = $decoder->decodeHexToByteArray($digestHex);
-        return $decoder->decodeLittleEndian($b, 512);
+        return $decoder->decodeLittleEndian($b, $this->byteSize*16);
     }
+
+    protected abstract function hash(string $hex): string;
 }
