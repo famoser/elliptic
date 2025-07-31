5d4f74b815d0564a038c2ff72202f0def40addd5
Florian Moser <git@famoser.ch>
2025-07-31 13:03:41 +0200

    test: Test ed25519 with third-party integration tests

---
+++ tests/integration/WycheProof/EddsaTest.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Famoser\Elliptic\Integration\WycheProof;
+
+use Famoser\Elliptic\Curves\BernsteinCurveFactory;
+use Famoser\Elliptic\Integration\Utils\ECDSASigner;
+use Famoser\Elliptic\Integration\Utils\EDDSASigner;
+use Famoser\Elliptic\Integration\WycheProof\Utils\FixturesRepository;
+use Famoser\Elliptic\Integration\WycheProof\Utils\WycheProofConstants;
+use Famoser\Elliptic\Math\MathInterface;
+use Famoser\Elliptic\Math\SW_ANeg3_Math;
+use Famoser\Elliptic\Math\SWUnsafeMath;
+use Famoser\Elliptic\Math\TwED_ANeg1_Math;
+use Famoser\Elliptic\Math\TwEDUnsafeMath;
+use Famoser\Elliptic\Primitives\Curve;
+use Famoser\Elliptic\Primitives\Point;
+use Famoser\Elliptic\Serializer\PointDecoder\PointDecoderException;
+use PHPUnit\Framework\TestCase;
+
+class EddsaTest extends TestCase
+{
+    public static function provideCurve25519(): array
+    {
+        return FixturesRepository::createEddsaFixtures('eddsa');
+    }
+
+    /**
+     * @dataProvider provideCurve25519
+     */
+    public function testEd25519UnsafeMath(string $public, string $message, string $signature, string $comment, string $result, array $flags): void
+    {
+        $math = new TwEDUnsafeMath(BernsteinCurveFactory::edwards25519());
+
+        $this->testEd25519($math, $public, $signature, $message, $result);
+    }
+
+    /**
+     * @dataProvider provideCurve25519
+     */
+    public function testEd25519ANeg1Math(string $public, string $message, string $signature, string $comment, string $result, array $flags): void
+    {
+        $math = new TwED_ANeg1_Math(BernsteinCurveFactory::edwards25519());
+
+        $signer = new EDDSASigner($math);
+        $verified = $signer->verify($public, $signature, $message);
+        if ($verified) {
+            $this->assertEquals($result, WycheProofConstants::RESULT_VALID);
+        } else {
+            $this->assertNotEquals($result, WycheProofConstants::RESULT_VALID);
+        }
+    }
+
+    public function testEd25519(MathInterface $math, string $public, string $signature, string $message, string $result): void
+    {
+        $signer = new EDDSASigner($math);
+        $verified = $signer->verify($public, $signature, $message);
+        if ($verified) {
+            $this->assertEquals($result, WycheProofConstants::RESULT_VALID);
+        } else {
+            $this->assertNotEquals($result, WycheProofConstants::RESULT_VALID);
+        }
+    }
+}
