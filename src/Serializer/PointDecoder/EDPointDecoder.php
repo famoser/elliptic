7dc4e06cfc62f96ea124f38e72bd7aaf6395ee90
Florian Moser <git@famoser.ch>
2025-07-31 14:56:59 +0200

    feat: Add EDPointDecoder

--- src/Serializer/PointDecoder/EDPointDecoder.php
+++ src/Serializer/PointDecoder/EDPointDecoder.php
@@ -4,9 +4,13 @@ namespace Famoser\Elliptic\Serializer\PointDecoder;
 
 use Famoser\Elliptic\Primitives\Curve;
 use Famoser\Elliptic\Primitives\CurveType;
+use Famoser\Elliptic\Primitives\Point;
 use Famoser\Elliptic\Serializer\PointDecoder\Traits\EdwardsPointDecoderTrait;
+use Famoser\Elliptic\Serializer\PointDecoder\Traits\FromCoordinatesTrait;
+use Famoser\Elliptic\Serializer\PointDecoder\Traits\FromXCoordinateTrait;
+use Famoser\Elliptic\Serializer\PointDecoder\Traits\FromYCoordinateTrait;
 
-class TwEDPointDecoder extends AbstractPointDecoder
+class EDPointDecoder extends AbstractPointDecoder
 {
     use EdwardsPointDecoderTrait;
 
@@ -15,7 +19,7 @@ class TwEDPointDecoder extends AbstractPointDecoder
         parent::__construct($curve);
 
         // check allowed to use this decoder
-        $check = $curve->getType() === CurveType::TwistedEdwards;
+        $check = $curve->getType() === CurveType::Edwards;
         if (!$check) {
             throw new \AssertionError('Cannot use this decoder with the chosen curve.');
         }
