526a010c753f76d9455c4d437dccbc2720be61ee
Florian Moser <git@famoser.ch>
2025-08-01 13:37:32 +0200

    test: Add timing collector

--- tests/consttime/MeasurementCollector.php
+++ tests/consttime/MeasurementCollector.php
@@ -2,89 +2,40 @@
 
 namespace Famoser\Elliptic\ConstTime;
 
-use Famoser\Elliptic\Curves\BrainpoolCurveFactory;
+use Famoser\Elliptic\ConstTime\Collectors\EcdsaSha256WycheProofCollector;
 use Famoser\Elliptic\Curves\CurveRepository;
 use Famoser\Elliptic\Math\MathInterface;
-use Famoser\Elliptic\Math\SW_ANeg3_Math;
-use Famoser\Elliptic\Math\SW_QT_ANeg3_Math;
 use Famoser\Elliptic\Math\SWUnsafeMath;
 
 class MeasurementCollector
 {
-    /**
-     * @return MathInterface[]
-     */
-    public function createMeasurementTargets(): array
+    public function collectMeasurements(): void
     {
-        $secp521r1 = self::getRepository()->findByName('secp521r1');
-        $p512r1 = self::getRepository()->findByName('brainpoolP512r1');
-        $p512r1Twist = BrainpoolCurveFactory::p512r1TwistToP512t1();
+        $curveRepository = new CurveRepository();
+        $curveName = 'secp192r1';
+        $math = new SWUnsafeMath($curveRepository->findByName($curveName));
 
-        return [
-            new SWUnsafeMath($secp521r1),
-            new SW_ANeg3_Math($secp521r1),
-            new SW_QT_ANeg3_Math($p512r1, $p512r1Twist),
-        ];
-    }
+        $proofCollector = new EcdsaSha256WycheProofCollector($curveName, $math);
+        $iteration = 0;
+        while (true) {
+            $proofCollector->collect();
+            $iteration++;
+            echo "Iteration: ".$iteration."\n";
 
-    public function collectMeasurements(MathInterface $math, int $sampleCount): void
-    {
-        [$baseline, $measurement] = $this->measureBaselineAgainstRandomPoint($math, $sampleCount);
-        $payload = ["baseline" => $baseline, "measurement" => $measurement];
-        $this->storeMeasurement("baseline_random", $math, $sampleCount, $payload);
-        ;
-    }
+            if ($iteration % 100 === 0) {
+                $output = $proofCollector->store();
+                $this->storeMeasurement($curveName, $math, $iteration, $output);
 
-    private static ?CurveRepository $curveRepository = null;
-
-    private static function getRepository(): CurveRepository
-    {
-        if (self::$curveRepository === null) {
-            self::$curveRepository = new CurveRepository();
+                echo "Stored.\n";
+            }
         }
-
-        return self::$curveRepository;
     }
 
-    private function measureBaselineAgainstRandomPoint(MathInterface $math, int $sampleCount): array
-    {
-        $fixedFactor = gmp_random_range(0, $math->getCurve()->getN());
-        $randomFactors = [];
-        for ($i = 0; $i < $sampleCount; $i++) {
-            $randomFactors[] = gmp_random_range(0, $math->getCurve()->getN());
-        }
-
-        $baseline = [];
-        $measurement = [];
-        for ($i = 0; $i < $sampleCount; $i++) {
-            $start = microtime(true);
-            $math->mul($math->getCurve()->getG(), $fixedFactor);
-            $end = microtime(true);
-            $baseline[] = $end - $start;
-
-            $variableFactor = $randomFactors[$i];
-            $start = microtime(true);
-            $math->mul($math->getCurve()->getG(), $variableFactor);
-            $end = microtime(true);
-            $measurement[] = $end - $start;
-        }
-
-        return [$baseline, $measurement];
-    }
-
-    private function storeMeasurement(string $measurementType, MathInterface $math, int $sampleCount, array $payload): void
+    private function storeMeasurement(string $curveName, MathInterface $math, int $sampleCount, array $payload): void
     {
         $mathName = substr($math::class, strrpos($math::class, '\\') + 1);
-        $curveName = self::getRepository()->getCanonicalName($math->getCurve());
-        $filename = $measurementType . "_" . $mathName . "_" . $curveName . "_" . $sampleCount . ".json";
+        $filename = $mathName . "_" . $curveName . "_" . $sampleCount . ".json";
         $path = __DIR__ . DIRECTORY_SEPARATOR . "measurements" . DIRECTORY_SEPARATOR . $filename;
-
-        $payload = [
-            "math" => $mathName,
-            "curve" => $curveName,
-            ...$payload
-        ];
-
         file_put_contents($path, json_encode($payload));
     }
 }
--- tests/consttime/collect.php
+++ tests/consttime/collect.php
@@ -4,11 +4,5 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 use Famoser\Elliptic\ConstTime\MeasurementCollector;
 
-// Script example.php
-$options = getopt("s::", ["samples::"]);
-$sampleCount = intval($options["samples"] ?? $options["s"] ?? 1000);
-
 $collector = new MeasurementCollector();
-foreach ($collector->createMeasurementTargets() as $measurementTarget) {
-    $collector->collectMeasurements($measurementTarget, $sampleCount);
-}
+$collector->collectMeasurements();
