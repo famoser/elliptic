c53e5465adfe738977e999e67b1311c56b3248ed
Florian Moser <git@famoser.ch>
2025-08-02 07:43:21 +0200

    test: Add more test collectors

--- tests/consttime/MeasurementCollector.php
+++ tests/consttime/MeasurementCollector.php
@@ -2,39 +2,62 @@
 
 namespace Famoser\Elliptic\ConstTime;
 
+use Famoser\Elliptic\ConstTime\Collectors\EcdsaSha256RooterbergProofCollector;
 use Famoser\Elliptic\ConstTime\Collectors\EcdsaSha256WycheProofCollector;
+use Famoser\Elliptic\ConstTime\Collectors\ProofCollectorInterface;
+use Famoser\Elliptic\Curves\BrainpoolCurveFactory;
 use Famoser\Elliptic\Curves\CurveRepository;
 use Famoser\Elliptic\Math\MathInterface;
+use Famoser\Elliptic\Math\SW_ANeg3_Math;
+use Famoser\Elliptic\Math\SW_QT_ANeg3_Math;
 use Famoser\Elliptic\Math\SWUnsafeMath;
 
 class MeasurementCollector
 {
-    public function collectMeasurements(): void
+    /**
+     * @return ProofCollectorInterface[]
+     */
+    private function proofCollectors(): array
     {
-        $curveRepository = new CurveRepository();
-        $curveName = 'secp192r1';
-        $math = new SWUnsafeMath($curveRepository->findByName($curveName));
+        $repo = new CurveRepository();
 
-        $proofCollector = new EcdsaSha256WycheProofCollector($curveName, $math);
+        return [
+            new EcdsaSha256WycheProofCollector('secp192r1', new SWUnsafeMath($repo->findByName('secp192r1'))),
+            new EcdsaSha256WycheProofCollector('secp192r1', new SW_ANeg3_Math($repo->findByName('secp192r1'))),
+            new EcdsaSha256RooterbergProofCollector(
+                'brainpool_p192r1',
+                224,
+                new SW_QT_ANeg3_Math($repo->findByName('brainpoolP192r1'), BrainpoolCurveFactory::p192r1TwistToP192t1())
+            ),
+        ];
+    }
+
+    public function collectMeasurements(int $collectorIndex): void
+    {
+        $proofCollector = $this->proofCollectors()[$collectorIndex];
+
+        echo "Starting measurement of collector with index " . $collectorIndex . ":\n";
+        echo $proofCollector->getMath()::class . " using " . $proofCollector->getCurveName() . "\n\n";
+
         $iteration = 0;
         while (true) {
             $proofCollector->collect();
             $iteration++;
-            echo "Iteration: ".$iteration."\n";
+            echo "Iteration: " . $iteration . "\n";
 
             if ($iteration % 100 === 0) {
                 $output = $proofCollector->store();
-                $this->storeMeasurement($curveName, $math, $iteration, $output);
+                $this->storeMeasurement($proofCollector->getMath(), $proofCollector->getCurveName(), $iteration, $output);
 
                 echo "Stored.\n";
             }
         }
     }
 
-    private function storeMeasurement(string $curveName, MathInterface $math, int $sampleCount, array $payload): void
+    private function storeMeasurement(MathInterface $math, string $curveName, int $iterationCount, array $payload): void
     {
         $mathName = substr($math::class, strrpos($math::class, '\\') + 1);
-        $filename = $mathName . "_" . $curveName . "_" . $sampleCount . ".json";
+        $filename = $mathName . "_" . $curveName . "_" . $iterationCount . ".json";
         $path = __DIR__ . DIRECTORY_SEPARATOR . "measurements" . DIRECTORY_SEPARATOR . $filename;
         file_put_contents($path, json_encode($payload));
     }
