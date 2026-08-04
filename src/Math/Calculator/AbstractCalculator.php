dbfe5e8757a2f303554703ddcb8aac4dab7c21ec
Florian Moser <git@famoser.ch>
2026-08-04 08:05:10 +0200

    doc: Document design decision

--- src/Math/Calculator/AbstractCalculator.php
+++ src/Math/Calculator/AbstractCalculator.php
@@ -13,6 +13,16 @@ abstract class AbstractCalculator
     public function __construct(protected readonly Curve $curve)
     {
         $this->field = new PrimeField($curve->getP());
+
+        /**
+         * reducing the factor by N would be enough, as this is the order of the group
+         * however, to defend against small-subgroup attacks (where the point is in the H subgroup instead of the N subgroup),
+         * some standards propose reducing the factor by N*H (see NIST Cofactor Diffie-Hellman)
+         * https://neilmadden.blog/2020/05/28/whats-the-curve25519-clamping-all-about/
+         *
+         * as H is 1 for all curves except bernsteins, it does not make a difference for all other curves
+         * therefore, we keep it here at N*H
+         */
         $this->nhField = new PrimeField(gmp_mul($curve->getN(), $curve->getH()));
     }
 }
