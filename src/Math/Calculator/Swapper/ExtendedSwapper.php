3ba173831451924f7df75d986844ba56d813cdda
Florian Moser <git@famoser.ch>
2026-08-05 09:04:04 +0200

    fix: Fix multiplication group

--- src/Math/Calculator/Swapper/ExtendedSwapper.php
+++ src/Math/Calculator/Swapper/ExtendedSwapper.php
@@ -13,6 +13,6 @@ trait ExtendedSwapper
         $this->conditionalSwapScalar($a->X, $b->X, $swapBit, $this->field->getElementBitLength());
         $this->conditionalSwapScalar($a->Y, $b->Y, $swapBit, $this->field->getElementBitLength());
         $this->conditionalSwapScalar($a->Z, $b->Z, $swapBit, $this->field->getElementBitLength());
-        $this->conditionalSwapScalar($a->T, $b->T, $swapBit, $this->field->getElementBitLength());
+        $this->conditionalSwapScalar($a->T, $b->T, $swapBit, $this->field->getElementBitLength() * 2); // T = x*y, hence need double the space
     }
 }
